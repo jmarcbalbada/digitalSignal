395764ace83a31d70b1264463d9f43db6a04f150
John Marc Balbada <91250096+jmarcbalbada@users.noreply.github.com>
2024-11-02 17:21:22 +0800

    fix: darkmode icon

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useColorScheme } from "@mui/joy/styles";
 import Button from "@mui/joy/Button";
+import ModeNightIcon from "@mui/icons-material/ModeNight";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 function DarkModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -17,14 +19,13 @@ function DarkModeToggle() {
       color="neutral"
       onClick={handleToggle}
       sx={{
-        // Apply margin based on the screen size
         marginRight: {
           xs: "0px", // No margin for extra small devices
           sm: "50px", // 50px margin for small devices and above
         },
       }}
     >
-      Switch to {mode === "light" ? "Dark" : "Light"} Mode
+      {mode === "light" ? <ModeNightIcon /> : <LightModeIcon />}
     </Button>
   );
 }
