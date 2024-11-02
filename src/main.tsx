9b6b2f90d0636ccf57b2314ce1ec655cc2561eed
John Marc Balbada <91250096+jmarcbalbada@users.noreply.github.com>
2024-11-02 17:12:06 +0800

    fix production

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { CssVarsProvider } from "@mui/joy/styles"; // Import the provider
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import theme from "./theme/theme"; // Import your custom theme
 
 createRoot(document.getElementById("root")!).render(
